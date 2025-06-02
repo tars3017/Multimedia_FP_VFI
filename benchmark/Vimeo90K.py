50ee618f6b133712d2c5f9e0b250115166a97bf9
tars3017 <prgmpclh@gmail.com>
2025-06-02 17:06:47 +0800

    Add evaluating pub_test code for end2end model

--- benchmark/Vimeo90K.py
+++ benchmark/Vimeo90K.py
@@ -47,7 +47,7 @@ model.device()
 print(f'=========================Starting testing=========================')
 print(f'Dataset: Vimeo90K   Model: {model.name}   TTA: {TTA}')
 path = args.path
-f = open(path + '/tri_trainlist.txt', 'r')
+f = open(path + '/tri_trainlist_complete.txt', 'r')
 psnr_list, ssim_list = [], []
 for i in f:
     name = str(i).strip().split(',')[0]
@@ -56,15 +56,46 @@ for i in f:
     I0 = cv2.imread(path + name + '/im1.png')
     I1 = cv2.imread(path + name + '/im2.png')
     I2 = cv2.imread(path + name + '/im3.png') # BGR -> RBG
-    I0 = (torch.tensor(I0.transpose(2, 0, 1)).cuda() / 255.).unsqueeze(0)
+    I3 = cv2.imread(path + name + '/im4.png')
+    I4 = cv2.imread(path + name + '/im5.png')
+    I5 = cv2.imread(path + name + '/im6.png')
+    I6 = cv2.imread(path + name + '/im7.png')
+
+    # Multiple Interpolation
+    # I0 = (torch.tensor(I0.transpose(2, 0, 1)).cuda() / 255.).unsqueeze(0)
+    # I1 = (torch.tensor(I1.transpose(2, 0, 1)).cuda() / 255.).unsqueeze(0)
+    # I2 = (torch.tensor(I2.transpose(2, 0, 1)).cuda() / 255.).unsqueeze(0)
+    # # I3 = (torch.tensor(I3.transpose(2, 0, 1)).cuda() / 255.).unsqueeze(0)
+    # I4 = (torch.tensor(I4.transpose(2, 0, 1)).cuda() / 255.).unsqueeze(0)
+    # I5 = (torch.tensor(I5.transpose(2, 0, 1)).cuda() / 255.).unsqueeze(0)
+    # I6 = (torch.tensor(I6.transpose(2, 0, 1)).cuda() / 255.).unsqueeze(0)
+    # mid1 = model.inference(I0, I1, TTA=TTA, fast_TTA=TTA)[0].unsqueeze(0)
+    # mid2 = model.inference(I5, I6, TTA=TTA, fast_TTA=TTA)[0].unsqueeze(0)
+    # mid3 = model.inference(mid1, I2, TTA=TTA, fast_TTA=TTA)[0].unsqueeze(0)
+    # mid4 = model.inference(mid2, I4, TTA=TTA, fast_TTA=TTA)[0].unsqueeze(0)
+    # mid = model.inference(mid3, mid4, TTA=TTA, fast_TTA=TTA)[0].unsqueeze(0)
+
+    # Single Interpolation
+    I2 = cv2.resize(I2, I3.shape[:2][::-1], interpolation=cv2.INTER_LINEAR)
+    I4 = cv2.resize(I4, I3.shape[:2][::-1], interpolation=cv2.INTER_LINEAR)
     I2 = (torch.tensor(I2.transpose(2, 0, 1)).cuda() / 255.).unsqueeze(0)
-    mid = model.inference(I0, I2, TTA=TTA, fast_TTA=TTA)[0]
-    ssim = ssim_matlab(torch.tensor(I1.transpose(2, 0, 1)).cuda().unsqueeze(0) / 255., mid.unsqueeze(0)).detach().cpu().numpy()
+    I4 = (torch.tensor(I4.transpose(2, 0, 1)).cuda() / 255.).unsqueeze(0)
+    mid = model.inference(I2, I4, TTA=TTA, fast_TTA=TTA)[0].unsqueeze(0)[0]
+
+
+    # mid = torch.nn.functional.interpolate(
+    #     mid,
+    #     size=(256, 448),
+    #     mode='bilinear',
+    #     align_corners=False
+    # )[0]
+
+    ssim = ssim_matlab(torch.tensor(I3.transpose(2, 0, 1)).cuda().unsqueeze(0) / 255., mid.unsqueeze(0)).detach().cpu().numpy()
     mid = mid.detach().cpu().numpy().transpose(1, 2, 0) 
     name_underline = name.replace('/', '_')
     cv2.imwrite(f'test_result/test_{name_underline}.jpg', (mid * 255).astype(np.uint8))
-    I1 = I1 / 255.
-    psnr = -10 * math.log10(((I1 - mid) * (I1 - mid)).mean())
+    I3 = I3 / 255.
+    psnr = -10 * math.log10(((I3 - mid) * (I3 - mid)).mean())
     psnr_list.append(psnr)
     ssim_list.append(ssim)
 
